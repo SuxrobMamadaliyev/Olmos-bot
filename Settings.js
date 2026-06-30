629107938e821817f5b389daffb90a842ea5cd1a
SuxrobMamadaliyev <mamadaliyevsuhrob69@gmail.com>
2026-06-30 20:27:46 +0500

    Create Settings.js

---
+++ Settings.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const settingsSchema = new mongoose.Schema(
+  {
+    key: { type: String, required: true, unique: true, default: 'main' },
+    paymentsChannel: { type: String, default: null },
+    referralReward: { type: Number, default: 250 },
+  },
+  { versionKey: false }
+);
+
+module.exports = mongoose.model('Settings', settingsSchema);
