91c101f871c8c76287fbdc8cc6eca975ace751d2
SuxrobMamadaliyev <mamadaliyevsuhrob69@gmail.com>
2026-07-01 10:39:29 +0500

    Update Settings.js

--- Settings.js
+++ Settings.js
@@ -5,6 +5,7 @@ const settingsSchema = new mongoose.Schema(
     key: { type: String, required: true, unique: true, default: 'main' },
     paymentsChannel: { type: String, default: null },
     referralReward: { type: Number, default: 250 },
+    minWithdraw: { type: Number, default: 1000 },
   },
   { versionKey: false }
 );
